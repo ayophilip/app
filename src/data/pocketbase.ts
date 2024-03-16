26e6e5e12a01421b26a515a30b2289bc954584b5
ayophilip <aayophilip@gmail.com>
2024-03-16 00:09:26 -0400

    add new tasks to project

--- src/data/pocketbase.ts
+++ src/data/pocketbase.ts
@@ -1,3 +1,31 @@
 import PocketBase from 'pocketbase'
 
-export const pb = new PocketBase('http://127.0.0.1:8090')
+// Initialize PocketBase to this
+export const pb = new PocketBase(import.meta.env.POCKETBASE_URL ||
+    process.env.POCKETBASE_URL)
+
+//Create a getProjects function that will retrieve the projects list
+export async function getProjects() {
+    const projects = await pb
+        .collection('projects')
+        .getFullList()
+    
+    return projects
+}
+
+//Create an addProject() function
+export async function addProject(name:string) {
+    const newProject = await pb.collection('projects')
+        .create({
+            name,
+            status: 'not started',
+        })
+    return newProject
+}
+
+// Fetch project from PocketBase
+export async function getProject(id:string) {
+    const project = await pb.collection('projects').getOne(id)
+    
+    return project
+}
